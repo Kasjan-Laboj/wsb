49f08570831423ba2ef02ed3e118c8f6980e0817
Kasjan-Laboj <kasjanlaboj@gmail.com>
2023-10-06 16:28:03 +0200

    new project

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,9 +2,26 @@
 {
     internal class Program
     {
+        static double Heron(double a,double b,double c)
+        {
+            double p = (a + b + c) / 2;
+            double s = Math.Sqrt(p*(p-a)*(p-b)*(p-c));
+            return s;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("podaj a, b ,c");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            if (a + b <=c || a+c<=b ||c+b<=a)
+            {
+                Console.WriteLine("nie ma takiego trojkata");
+            }
+            else Console.WriteLine(Heron(a, b, c));
+
+
         }
     }
 }
